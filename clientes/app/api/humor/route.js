2665aaa18a96f391c30dc225e0daa5ac589cfc2d
Otavio Mastrantonio <111095614+tavilom@users.noreply.github.com>
2025-06-01 16:14:05 -0300

    Update route.js
    Alterado o route.js para se comunicar com o db.js

--- clientes/app/api/humor/route.js
+++ clientes/app/api/humor/route.js
@@ -9,13 +9,35 @@ export async function POST(req) {
   }
 
   try {
+    // Busca se já há envio no mesmo dia (formato YYYY-MM-DD)
+    const hoje = new Date().toISOString().slice(0, 10);
+
+    const resultado = await db.get(
+      `SELECT * FROM humores 
+       WHERE DATE(criado_em) = ?`,
+      [hoje]
+    );
+
+    if (resultado) {
+      return NextResponse.json(
+        { error: 'Você já enviou seu humor hoje.' },
+        { status: 400 }
+      );
+    }
+
+    // Cria um novo registro de humor
     await db.run(
       'INSERT INTO humores (humor, criado_em) VALUES (?, datetime("now"))',
       [humor]
     );
+
     return NextResponse.json({ message: 'Humor salvo com sucesso.' });
   } catch (error) {
-    return NextResponse.json({ error: 'Erro ao salvar humor.' }, { status: 500 });
+    console.error(error);
+    return NextResponse.json(
+      { error: 'Erro ao salvar humor.' },
+      { status: 500 }
+    );
   }
 }
 
